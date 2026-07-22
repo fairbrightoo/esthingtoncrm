cfaccc82d4d75322e018cb3c2bc1fc7c6f234624
Bright Osisiogu <fairbrightoo@gmail.com>
2026-07-22 16:04:41 +0100

    fix(backend): increase multer upload limit to 50MB to fix failed to record payment error for large high-res camera photos

--- backend/src/middleware/uploadMiddleware.ts
+++ backend/src/middleware/uploadMiddleware.ts
@@ -5,5 +5,5 @@ const storage = multer.memoryStorage();
 
 export const upload = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+    limits: { fileSize: 50 * 1024 * 1024 } // 50MB limit
 });
