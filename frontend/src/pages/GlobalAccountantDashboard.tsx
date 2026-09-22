68772d746d94867163a0d45ccb4278e4a47703ba
Bright Osisiogu <fairbrightoo@gmail.com>
2026-09-22 13:12:19 +0100

    fix: correctly close JSX div in GlobalAccountantDashboard

--- frontend/src/pages/GlobalAccountantDashboard.tsx
+++ frontend/src/pages/GlobalAccountantDashboard.tsx
@@ -83,6 +83,7 @@ export const GlobalAccountantDashboard = () => {
                         ))}
                     </select>
                 </div>
+            </div>
             )}
 
             {/* Main Content Area */}
